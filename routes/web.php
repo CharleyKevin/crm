dc125f233f8a399f80b63b77455cf5075f09cdcc
Charley Kevin <charleyk05@gmail.com>
2024-04-21 00:18:20 -0300

    crm-2: creating login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Livewire\Auth\Register;
+use App\Livewire\Auth\{Login, Register};
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Welcome::class)->name('welcome');
+Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => auth()->logout())->name('auth.logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', Welcome::class)->name('welcome');
+});
